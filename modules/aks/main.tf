345b5c7c6d4cb92d5b99620228c46f102c0d55d5
chiju <chijumelveettil@gmail.com>
2025-11-15 09:44:28 +0100

    Fix Azure AD RBAC syntax for provider 4.x

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -7,6 +7,11 @@ resource "azurerm_kubernetes_cluster" "main" {
   oidc_issuer_enabled               = true
   role_based_access_control_enabled = true
 
+  azure_active_directory_role_based_access_control {
+    admin_group_object_ids = []
+    azure_rbac_enabled     = true
+  }
+
   default_node_pool {
     name           = "default"
     node_count     = var.node_count
