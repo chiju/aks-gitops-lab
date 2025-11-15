da897d78309cd49ce91bf86bd2f63d64114cd6b6
chiju <chijumelveettil@gmail.com>
2025-11-15 17:34:39 +0100

    Add prometheus monitoring and cleanup script

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-state-rg"
-    storage_account_name = "tfstatec9546223"
+    storage_account_name = "tfstate27a151e5"
     container_name       = "tfstate"
     key                  = "aks-gitops-lab.tfstate"
   }
