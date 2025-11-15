414ae5209e758083b951c553d7ccbe4a66c49596
chiju <chijumelveettil@gmail.com>
2025-11-15 15:20:43 +0100

    Fresh start with full automation - subscription-level permissions

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-state-rg"
-    storage_account_name = "tfstate65ec534e"
+    storage_account_name = "tfstatebc11c227"
     container_name       = "tfstate"
     key                  = "aks-gitops-lab.tfstate"
   }
