968a82aee3564db75d9e58844b5426eb6f6a6da4
chiju <chijumelveettil@gmail.com>
2025-11-15 09:27:04 +0100

    Fix Azure AD RBAC configuration and formatting

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -1,11 +1,15 @@
 resource "azurerm_kubernetes_cluster" "main" {
-  name                = var.name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  dns_prefix          = var.dns_prefix
-  kubernetes_version  = var.kubernetes_version
-  oidc_issuer_enabled = true
+  name                              = var.name
+  location                          = var.location
+  resource_group_name               = var.resource_group_name
+  dns_prefix                        = var.dns_prefix
+  kubernetes_version                = var.kubernetes_version
+  oidc_issuer_enabled               = true
   role_based_access_control_enabled = true
+  azure_active_directory_role_based_access_control {
+    managed            = true
+    azure_rbac_enabled = true
+  }
 
   default_node_pool {
     name           = "default"
@@ -16,11 +20,6 @@ resource "azurerm_kubernetes_cluster" "main" {
 
   identity {
     type = "SystemAssigned"
-  }
-
-  azure_active_directory_role_based_access_control {
-    managed = true
-    azure_rbac_enabled = true
   }
 
   network_profile {
