701e363f1b1e4b1634fbf36bf83c0041b8619db9
chiju <chijumelveettil@gmail.com>
2025-11-16 15:56:03 +0100

    Fix: Remove node_count when auto_scaling_enabled is true

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -13,13 +13,12 @@ resource "azurerm_kubernetes_cluster" "main" {
   }
 
   default_node_pool {
-    name                = "default"
-    vm_size             = var.vm_size
-    vnet_subnet_id      = var.subnet_id
-    enable_auto_scaling = true
-    min_count           = var.min_node_count
-    max_count           = var.max_node_count
-    node_count          = var.node_count
+    name                 = "default"
+    vm_size              = var.vm_size
+    vnet_subnet_id       = var.subnet_id
+    auto_scaling_enabled = true
+    min_count            = var.min_node_count
+    max_count            = var.max_node_count
   }
 
   identity {
